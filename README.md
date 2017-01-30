750ef6a96bf52ec3687075fe14a9f14f4f3b2e3c
Carsten Magnus <carsten.magnus@bsse.ethz.ch>
2017-01-30 13:23:07 +0100

    first commit, all files added

---
+++ README.md
@@ -0,0 +1,2 @@
+# StopHIVInf
+Design a webtool to predict protective antibody levels against HIV infection
---
+++ templates/projects.js
@@ -0,0 +1,40 @@
+projects = [
+	{
+		"name" : "Cellosaurus Web-Tool and Wikidata bot",
+		"desc" : "The students should develop a web tool to compare STR Marker profile for a cell line and a Wikidata bot",
+		"issues" : [
+			"https://github.com/calipho-sib/cellosaurus/issues"
+		],
+		"mentors" : [
+			"ddtxra",
+			"vrechdelaval"
+		],
+		"keywords" : ["cell line", "ExPasy"],
+		"links" : ["http://web.expasy.org/cellosaurus/", "http://web.expasy.org/cellosaurus/description.html", "https://github.com/calipho-sib/cellosaurus", "https://github.com/calipho-sib/cellosaurus-syntax-checker"],
+		"skills" : ["scala", "python", "java", "rdf", "git"],
+		"markdown": "cellosaurus.md"
+	},
+	{
+		"name" : "SugarBuilder",
+		"desc" : "Design an intuitive online interface for drawing Glycan structures on the web",
+		"issues" : [
+			"https://github.com/alodavide/sugarBuilder"
+		],
+		"mentors" : [
+			"alodavide","julienmariethoz"
+		],
+		"keywords" : ["Glycomics", "ExPASy", "sugars", "carbohydrates", "javascript", "web-interface", "drawing tool"],
+		"links" : ["http://www.expasy.org/glycomics", "http://www.sib.swiss/lisacek-frederique/","https://github.com/alodavide/sugarBuilder","http://glycopedia.eu/"],
+		"skills" : ["creativity", "design", "javascript", "D3js", "Google Polymer"],
+		"markdown": "sugarbuilder.md"
+	},
+	{
+		“name” : “StopHIVInf”,
+		“desc” : “Design a web tool to predict protective antibody levels against HIV infection”,
+		“issues” : [
+			“https://github.com/cmagnus/StopHIVInf”
+		]
+	}
+
+	
+]
